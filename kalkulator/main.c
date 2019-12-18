2be9e45550e838a20375bc79d83d8b90b7de7d98
ETF Student 1 <etf1@mail.com>
2019-12-18 14:45:43 +0100

    funkcija za racunanje faktorijela nekog broja

--- kalkulator/main.c
+++ kalkulator/main.c
@@ -6,8 +6,19 @@ int main()
     printf("Hello world!\n");
     return 0;
 }
+<<<<<<< HEAD
 
 int sabiranje (int a, int b)
 {
 	return a+b;
+}
+int faktorijel(int num){
+    int f=1;
+    int i;
+    for(i=1;i<=num;i++){
+        f=f*i;
+    }
+    return f;
+}
+>>>>>>> funkcija za racunanje faktorijela nekog broja
 }
