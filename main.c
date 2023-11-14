30c122380855a3cc24d48102d66074a43b066a45
PC-21\Student <student@etf.bl.net>
2023-11-14 13:54:21 +0100

    no message

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
-int main();
+#include <stdio.h>
+#include <math.h>
+#include "najmanjiOdNbr.h"
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
+    printf("%d",najmanji(n,arr));
+}
