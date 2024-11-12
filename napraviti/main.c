c30192a07a2579ccfaa7683e877af89e2efabcbf
PC06\Student <student@etf.net>
2024-11-12 13:47:16 +0100

    Racunanje procenta broja

--- napraviti/main.c
+++ napraviti/main.c
@@ -2,6 +2,12 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    float n, percentage, result;
+    printf("Unesite broj:");
+    scanf("%f", &n);
+    printf("Unesite procenat:");
+    scanf("%f", &percentage);
+    result = n*(percentage/100);
+    printf("Procenat broja je: %0.2f.", result);
     return 0;
 }
