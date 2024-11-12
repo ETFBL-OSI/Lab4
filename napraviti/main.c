19524b7d11b1afc4361b5e7026d592af1f2529b9
PC02\Student <student@etfbl.net>
2024-11-12 13:43:19 +0100

    Popravka 2 maina

--- napraviti/main.c
+++ napraviti/main.c
@@ -9,31 +9,25 @@ int racunanjestatka(int broj, int djelilac);
 
 int main()
 {
-
-    int ostatak = racunanjeOstatka(10, 3);
-    /*
-    double a = 2;
-    double b = 3;
-    double c = stepenovanje(a,b);
-    printf("%lf", c);
-    */
-
-}
-int sabiranje2Broja(int a, int b)
-{
-    return a+b;
-}
-
-int main()
-{ int a,b;
+    int a,b;
 
     printf("Hello world!\n");
     printf("Unesite 2 broja:");
     scanf("%d %d",&a,&b);
     printf("Suma 2 broja:%d",sabiranje2Broja(a,b));
+    int ostatak = racunanjeOstatka(10, 3);
+    /*
+    double aa = 2;
+    double bb = 3;
+    double cc = stepenovanje(a,b);
+    printf("%lf", c);
+*/
     return 0;
 }
-
+int sabiranje2Broja(int a, int b)
+{
+    return a+b;
+}
 int racunanjeOstatka(int broj, int djelilac){
     int ostatak;
     ostatak = broj % djelilac;
