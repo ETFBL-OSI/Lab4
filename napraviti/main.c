0bae62741149db5249ce0bfba43f390e2f5fee45
PC05\Student <viviana-anna.pajic@student.etf.unibl.org>
2024-11-12 13:44:02 +0100

    Faktorijel broja

--- napraviti/main.c
+++ napraviti/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+int faktorijel(int broj){
+
+    int p = 1;
+    for( int i = 1; i <= broj; i++){
+        p *= i;
+    }
+
+    return p;
+}
+
 int main()
 {
     printf("Hello world!\n");
+
+    int f = faktorijel(4);
+    printf("%d", f);
+
     return 0;
 }
