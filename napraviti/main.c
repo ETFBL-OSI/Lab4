6f894582a09f7f9ae8f2913694ff7ee0a4f7fc2f
PC04\Student <studentemail@email.com>
2024-11-12 13:40:09 +0100

    dobijanje ostatka pri cjelobrojnom dijeljenju

--- napraviti/main.c
+++ napraviti/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+int racunanjestatka(int broj, int djelilac);
+
+
 int main()
 {
+
+
+    int ostatak = racunanjeOstatka(10, 3);
+
     printf("Hello world!\n");
     return 0;
+}
+
+int racunanjeOstatka(int broj, int djelilac){
+    int ostatak;
+    ostatak = broj % djelilac;
+    return ostatak;
 }
