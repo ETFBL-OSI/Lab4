2018be7cc773e4fef5486680486037221e63502e
PC03\Student <studentetfbl@etf.bl.net>
2024-11-12 13:40:51 +0100

    Oduzimanje dva broja

--- napraviti/main.c
+++ napraviti/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+int oduzimanjeDvaBroja(int a, int b)
+{
+    return a-b;
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    int a, b;
+    printf("Unesi dva broja: ");
+    scanf("%d %d", &a, &b);
+    int x = oduzimanjeDvaBroja(a,b);
+    printf("Razlika dva unesena broja %d i %d je: %d", a,b,x);
     return 0;
 }
