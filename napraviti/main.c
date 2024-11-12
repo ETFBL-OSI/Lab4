bc1bcc1c52198dcb0754eb614a9a939796892ed8
PC10\Student <student@etfbl.net>
2024-11-12 13:37:21 +0100

    no message

--- napraviti/main.c
+++ napraviti/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+int sabiranje2Broja(int a, int b)
+{
+    return a+b;
+}
 
 int main()
-{
+{ int a,b;
+
     printf("Hello world!\n");
+    printf("Unesite 2 broja:");
+scanf("%d %d",&a,&b);
+    printf("Suma 2 broja:%d",sabiranje2Broja(a,b));
     return 0;
 }
